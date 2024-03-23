3f4747d846870150250982be66560dbc17ff415e
JNyaga <joelnyagamuriithi@gmail.com>
2024-03-23 07:21:27 +0300

    Add Course class and update CoureJdbcRepository and CourseJdbcCommandLineRunner

--- src/main/java/com/ropro/jpahibernate/course/jdbc/CoureJdbcRepository.java
+++ src/main/java/com/ropro/jpahibernate/course/jdbc/CoureJdbcRepository.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.ropro.jpahibernate.course.Course;
+
 @Repository
 public class CoureJdbcRepository {
 
@@ -12,12 +14,21 @@ public class CoureJdbcRepository {
 
     private static String INSERT_QUERY = """
             insert into COURSE (id, name, author)
-            values (1, 'learn AWS', 'Joel');
+            values (?, ? , ?);
 
                   """;
 
-    public void insert() {
-        springJdbcTemplate.update(INSERT_QUERY);
+    private static String DELETE_QUERY = """
+            delete from COURSE where id = ?;
+
+                  """;
+
+    public void insert(Course course) {
+        springJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void deleteById(long id) {
+        springJdbcTemplate.update(DELETE_QUERY, id);
     }
 
 }
--- src/main/java/com/ropro/jpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/ropro/jpahibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.ropro.jpahibernate.course.Course;
+
 // runs at startup
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
@@ -13,7 +15,8 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn Aws Now", "Joel"));
+        repository.deleteById(1);
     }
 
 }
